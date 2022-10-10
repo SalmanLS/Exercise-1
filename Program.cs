fadfb62521139333385d3332a5c857861d839cef
SalmanLS <salmanluthfi456@gmail.com>
2022-10-10 15:51:32 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercise
+{
+    class Program
+    {
+        private int[] SLS = new int[29];
+        private int n;
+
+        public void Read()
+        {
+            while (true) 
+            {  
+                Console.WriteLine("Masukkan banyaknya elemen pada array: ");
+                string s = Console.ReadLine();
+                n = int.Parse(s);
+                if (n <= 29)
+                    break;
+                else
+                    Console.WriteLine("\n Maksimal elemen pada array adalah 29 \n");
+        }
+    }
+}
